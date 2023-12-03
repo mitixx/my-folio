9a7f9efa3bbf9beedc46c563883134cef3613eb9
mitixx <haruokame05@yahoo.co.jp>
2023-12-03 18:28:24 +0900

    add:目次・見出し・メニュー

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>未知's Portfolio</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/mitixx/',
   },
-  chat: {
+ /* chat: {
     link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  }, */
+  docsRepositoryBase: 'https://github.com/mitixx/my-folio/',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Mitixx folio',
   },
 }
 
