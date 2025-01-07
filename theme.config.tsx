af8b6e1d2ffec8817312228929eaddaf20d3ef8d
mitixx <haruokame05@yahoo.co.jp>
2025-01-07 22:33:17 +0900

    add:X link,技術スキルの更新

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>未知's Portfolio</span>,
+  logo: <span>darupu's Portfolio</span>,
   project: {
     link: 'https://github.com/mitixx/',
   },
- /* chat: {
-    link: 'https://discord.com',
+  /* chat: {
+    link: '',
   }, */
   docsRepositoryBase: 'https://github.com/mitixx/my-folio/',
   footer: {
-    text: 'Mitixx folio',
+    text: 'darupu folio',
   },
 }
 
